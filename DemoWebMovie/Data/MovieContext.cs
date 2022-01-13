77e74972dc17f60521e7e68750137ba3476970ef
Roni Kurniawan <rokurnia@microsoft.com>
2022-01-13 12:18:19 +0700

    Add project files.

---
+++ DemoWebMovie/Data/MovieContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DemoWebMovie.Models;
+
+namespace DemoWebMovie.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DemoWebMovie.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ DemoWebMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DemoWebMovie.Data;
+using DemoWebMovie.Models;
+
+namespace DemoWebMovie.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DemoWebMovie.Data.MovieContext _context;
+
+        public IndexModel(DemoWebMovie.Data.MovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
